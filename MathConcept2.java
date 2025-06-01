d66652d0f659dd0d41eefa5b44ef78e8c4a113d8
AnuVasudevan24 <125848706+AnuVasudevan24@users.noreply.github.com>
2025-06-01 22:31:35 +0200

    Add files via upload

---
+++ MathConcept2.java
@@ -0,0 +1,13 @@
+class MathConcept2 {
+    public static void main(String[] args) {
+        int n=9879,count = 0;
+        
+        while(n>0){
+            int ld=n%10;
+            count = count + 1;
+            n=n/10;
+        }
+        
+    System.out.println(count);
+    }
+}
